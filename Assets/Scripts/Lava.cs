0a803379dd06a9d0dd677dd4bed63ef53196b2b1
CinnamonBagels <samuelsko@gmail.com>
2014-04-19 16:07:14 -0700

    working playerDamage

--- Assets/Scripts/Lava.cs
+++ Assets/Scripts/Lava.cs
@@ -4,30 +4,8 @@ using System.Collections;
 public class Lava : MonoBehaviour {
 
 	private float damage = 5.0f;
-	private bool isColliding = false;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Debug.Log(isColliding);
-	}
-
-	void OnCollisionEnter(Collision col) {
-		isColliding = true;
-	}
-
-	void OnCollisionStay(Collision col) {
-		if (col.gameObject.name == "Player") {
-			var pHealth = col.gameObject.GetComponent<PlayerHealth>();
-			pHealth.dealDamage(damage);
-		}
-	}
-
-	void OnCollisionExit(Collision col) {
-		isColliding = false;
+	public float getDamage() {
+		return damage;
 	}
 }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,24 +4,47 @@ using System.Collections;
 public class PlayerHealth : MonoBehaviour {
 
 	public float maxHealth = 100.0f;
+	public float damageCD = 1.0f;
 
+	private float currentDamageCD;
 	private float currentHealth;
+	private bool canTakeDamage;
 
 	// Use this for initialization
 	void Start () {
+		currentDamageCD = damageCD;
+		canTakeDamage = true;
 		currentHealth = maxHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//Debug.Log(currentHealth);
+		Debug.Log(currentHealth);
+		currentDamageCD -= Time.deltaTime;
+
+		if (currentDamageCD <= 0) {
+			canTakeDamage = true;
+			currentDamageCD = damageCD;
+		}
 	}
 
-	public void dealDamage(float damage) {
-		currentHealth -= damage;
+	public void receiveDamage(float damage) {
+		if (canTakeDamage) {
+			currentHealth -= damage;
 
-		if (currentHealth <= 0.0f) {
-			die();
+			canTakeDamage = false;
+
+			if (currentHealth <= 0.0f) {
+				die();
+			}
+		}
+	}
+
+	public void heal(float healAmount) {
+		currentHealth += healAmount;
+
+		if (currentHealth >= maxHealth) {
+			currentHealth = maxHealth;
 		}
 	}
 
