7753a7a0b33e9e55214627ea555f66fd7862d777
CinnamonBagels <samuelsko@gmail.com>
2014-04-19 14:01:27 -0700

    init commit

---
+++ Assets/Scripts/Lava.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lava : MonoBehaviour {
+
+	private float damage = 5.0f;
+	private bool isColliding = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(isColliding);
+	}
+
+	void OnCollisionStay(Collision col) {
+
+		if (col.gameObject.name == "Player") {
+			var pHealth = col.gameObject.GetComponent<PlayerHealth>();
+			pHealth.dealDamage(damage);
+		}
+		isColliding = true;
+	}
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public float maxHealth = 100.0f;
+
+	private float currentHealth;
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log(currentHealth);
+	}
+
+	public void dealDamage(float damage) {
+		currentHealth -= damage;
+
+		if (currentHealth <= 0.0f) {
+			die();
+		}
+	}
+
+	private void die() {
+		Destroy(this.gameObject);
+	}
+}
